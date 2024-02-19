01c0f94c9231d371f6064ad86a34c51cff4ef9ea
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2024-02-19 17:16:55 +0400

    variablesAdded

--- main.cpp
+++ main.cpp
@@ -4,6 +4,29 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    //dworing a shape samkutxedi
+
+    cout << "   /|" << endl;
+    cout << "  / |" << endl;
+    cout << " /  |" << endl;
+    cout << "/___|" << endl;
+
+    //Variables in C++
+
+    string name ="Rene";
+    int age = 25;
+
+    cout << "There once was a man named " << name << endl;
+    cout << "He was "<< age <<" years old" << endl;
+
+    name = "Tom";
+    cout << "He liked the name " << name << endl;
+    cout << "But didnt liked being " << age << endl;
+
+
+    // Data tipes
+
+
+
     return 0;
 }
